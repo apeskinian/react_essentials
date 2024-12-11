33d6fac273bc0ee6455b10f304090bf22e440d86
Matthew Aitken <matt@itken.net>
2024-12-11 14:29:49 +0000

    Add tabContent with dynamic info

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
 import  { CORE_CONCEPTS } from './data.js';
+import  { EXAMPLES } from './data.js';
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
 
-  let tabContent = 'Please click a button';
+  const [ selectedTopic, setSelectedTopic ] = useState();
+
   function handleSelect(selectedButton) {
-    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
