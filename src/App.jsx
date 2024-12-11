3851e6c97dbec1558b31d86d6c28ea6712689078
Matthew Aitken <matt@itken.net>
2024-12-11 10:28:48 +0000

    Add concepts component
    This is reused 4 times and filled dynamically from data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import ReactCoreConcepts from "./assets/react-core-concepts.png";
+import reactCoreConcepts from "./assets/react-core-concepts.png";
+import  { CORE_CONCEPTS } from './data.js';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = [
+  "Essential", "Core", "Primary", "Central", "Elementary",
+  "Foundational", "Underlying", "Principal", "Key", "Crucial",
+  "Vital", "Important", "Indispensable", "Integral", "Critical",
+  "Necessary", "Pivotal", "Cardinal", "Substantial", "Rudimentary"
+];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
+  const description = reactDescriptions[genRandomInt(19)]
   return (
     <header>
-      <img src={ReactCoreConcepts} alt="Stylized atom" />
+      <img src={reactCoreConcepts} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -20,12 +26,38 @@ function Header() {
   );
 }
 
+// the function parameter below uses destructuring to get the parameters
+// from the sent props
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* Longer option of sending props */}
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            {/* Shorter way to send props by using spread operator */}
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
