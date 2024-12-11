3d7ffe4cf3a496c31dc77925b152a47410ac1705
Matthew Aitken <matt@itken.net>
2024-12-11 15:06:49 +0000

    Output list content dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -36,16 +36,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {/* Longer option of sending props */}
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            {/* Shorter way to send props by using spread operator */}
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
