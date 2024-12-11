d6fb83a3c03ffa6207c41b473bdba64d8e6393b4
Matthew Aitken <matt@itken.net>
2024-12-11 12:28:37 +0000

    Change tabbutton onClick event

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+
+  let tabContent = 'Please click a button';
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+  }
+
   return (
     <div>
       <Header />
@@ -26,11 +33,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
