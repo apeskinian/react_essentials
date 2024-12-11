f8ef2676bb578ffaedfd7d3dc4b8e9850f1e806c
Matthew Aitken <matt@itken.net>
2024-12-11 09:41:46 +0000

    Add dynamically changing description

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import ReactCoreConcepts from './assets/react-core-concepts.png';
+import ReactCoreConcepts from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)]
+  return (
+    <header>
+      <img src={ReactCoreConcepts} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <img src={ReactCoreConcepts} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
